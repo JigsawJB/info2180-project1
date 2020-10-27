598bd7f3a3b6a420acc03778c976767a7b675d82
Jaun-Luc <jaunlucbrown15@gmail.com>
2020-10-26 23:13:18 -0500

    Sprinkle of Js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-/* Add your JavaScript to this file */
+//js code to validate form
+window.addEventListener('load' , () => {
+
+    const $email = document.getElementById('email');
+    const $sub_form = document.getElementsByTagName("button")[0];
+    const $alert = document.getElementsByClassName("message")[0];
+
+    $sub_form.addEventListener('click' , (e) => {
+        e.preventDefault();
+
+        $email.value === null || $email.value === "" ? $alert.innerHTML = "Please enter a valid email address." : 
+        
+        $alert.innerHTML = "Thank you! Your email address, " + $email.value + " has been added to our mailing list.";
+    })
+})
